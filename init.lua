a4ed6f1adac1346dbe0b178ebd4749b698681f27
Z0SO <lautarorobales31@gmail.com>
2024-08-19 23:07:47 -0300

    Agregado -> options.lua

--- init.lua
+++ init.lua
@@ -4,29 +4,8 @@
 vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/site/pack/packer/start/lazy.nvim')
 
 require('core.keymaps')
-
+require('core.options')
 
 -- Configura lazy.nvim para gestionar plugins
 require('lazy').setup('plugins')
-
-
-
-
-
-
--- Otras configuraciones globales pueden ir aquí
--- Por ejemplo, configuración de opciones globales, mapeos de teclas, etc.
-
--- Ejemplo de configuración adicional (opcional)
--- Configura la interfaz de usuario, ajustes globales, etc.
-
-
-
-
--- Opcional: Configura opciones básicas
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
 
